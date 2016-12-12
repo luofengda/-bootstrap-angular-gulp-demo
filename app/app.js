a28ccf050225e7ea9a7b3ad355b1b44e5fe6b439
luofengda <362861693@qq.com>
2016-12-12 21:55:28 +0800

    正在热映 板块可以获取本地的测试数据

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@
     // "use strict";
     // start your ride
    	angular.module('moviecat',
-   		['moviecat.home','moviecat.in_theaters']);
+   		['moviecat.home','moviecat.in_theaters','moviecat.in_theaters','moviecat.top250']);
 
 })(angular);
--- app/in_theaters/module.js
+++ app/in_theaters/module.js
@@ -2,7 +2,7 @@
 * @Author: luofengda
 * @Date:   2016-12-12 17:06:00
 * @Last Modified by:   luofengda
-* @Last Modified time: 2016-12-12 20:06:18
+* @Last Modified time: 2016-12-12 21:39:46
 */
 
 (function(angular){
@@ -11,10 +11,19 @@
 	.config(['$routeProvider',function($routeProvider) {
 		$routeProvider.when('/in_theaters',{
 			templateUrl:'./in_theaters/view.html',
-			contorller:'InTheatersContorller'
+			controller:'InTheatersContorller'
 		});
-	}]).controller('InTheatersController', ['$scope', function($scope){
-		
+	}]).controller('InTheatersContorller', ['$scope','$http', function($scope,$http){
+		 
+		$http({
+			method:'GET',
+			url:'./in_theaters/datas.json'
+		}).then(function(response){
+			console.log(response);
+			$scope.movie=response.data;
+		}, function(response){
+
+		})
 	}])
 
 
