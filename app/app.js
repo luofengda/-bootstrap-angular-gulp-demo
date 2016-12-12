4262767d410a82fb616a0b80f98809797eca4da8
luofengda <362861693@qq.com>
2016-12-12 21:35:26 +0800

    修复上个版本中 contorller中 出获取不到数据的问题

--- app/app.js
+++ app/app.js
@@ -2,10 +2,6 @@
     // "use strict";
     // start your ride
    	angular.module('moviecat',
-   		['moviecat.home',
-   		'moviecat.in_theaters',
-   		'moviecat.coming_soon',
-   		'moviecat.top250'
-   		]);
+   		['moviecat.home','moviecat.in_theaters']);
 
 })(angular);
